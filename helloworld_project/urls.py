2bf6d044e56b3902555e9e183b5861d175e9d345
Yukikoxoxo <yukikoishiguro.python@gmail.com>
2025-03-06 17:22:08 +0900

    0606_1721_クラスベースビューを関数ベースビューに変換Helloworld2成功

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import helloworldfunction
+from .views import helloworldfunction, HelloWorldClass
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("helloworld/", helloworldfunction),
+    path("helloworld2/",  HelloWorldClass.as_view()),
+    # クラスベースビューを関数ベースビューに変換
+    # as_view()を使うことで、クラスベースビューを関数ベースビューに変換している!
 ]
