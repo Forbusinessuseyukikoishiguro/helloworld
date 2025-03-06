29893e227ef5d4fe11b5ff13c8baacb33262005d
Yukikoxoxo <yukikoishiguro.python@gmail.com>
2025-03-06 18:28:48 +0900

    helloworldapp作成

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "helloworldapp.apps.HelloworldappConfig",
+                # テンプレートファイルに変数を渡すための設定
             ],
         },
     },
