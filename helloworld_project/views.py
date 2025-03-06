fbf565da74ac9401dc6acbb713b28e285342b7ff
Yukikoxoxo <yukikoishiguro.python@gmail.com>
2025-03-06 15:06:59 +0900

    継承とテンプレートネーム

--- helloworld_project/views.py
+++ helloworld_project/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 def helloworldfunction(request):
     returnedobject = HttpResponse("<h1>Hello World</h1>")
     return returnedobject
+
+
+# Class based views  Viewの継承
+class HelloWorldClass(TemplateView):
+    template_name = "helloworld.html"
+    # テンプレートファイルの指定
