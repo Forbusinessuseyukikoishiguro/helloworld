1ca7a8904439199d90c19f7e29a60d587bc15881
Yukikoxoxo <yukikoishiguro.python@gmail.com>
2025-03-06 18:45:36 +0900

    1548繋ぎこみ

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .views import helloworldfunction, HelloWorldClass
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("helloworld/", helloworldfunction),
-    path("helloworld2/",  HelloWorldClass.as_view()),
+    path("helloworld2/", HelloWorldClass.as_view()),
     # クラスベースビューを関数ベースビューに変換
     # as_view()を使うことで、クラスベースビューを関数ベースビューに変換している!
+    path("helloworldapp/", include("helloworldapp.urls")),
 ]
